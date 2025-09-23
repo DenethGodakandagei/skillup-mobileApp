a4df097e01577a4131e1818bd6b98d797c616980
Deneth  Godakandagei <contact.deneth@gmail.com>
2025-09-23 13:18:29 +0530

    bug fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import InitialLayout from "@/components/InitialLayout";
 
-import { AppProvider } from "@/context/AppContext";
 import { COLORS } from "@/constants/theme";
 import ClerkAndConvexProvider from "@/providers/ClerkAndConvexProvider";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
@@ -18,10 +17,6 @@ export default function RootLayout() {
   return(
     <ClerkAndConvexProvider>
       <SafeAreaProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: "#fff" }}>
-          <AppProvider>
-            <InitialLayout/>
-          </AppProvider>
         <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
           <InitialLayout>
           </InitialLayout>
